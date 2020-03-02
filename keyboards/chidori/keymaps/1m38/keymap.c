5e126badd3ffc9c9897cf862e82fcaeb36e94d68
Masaya Uno <13414029+1m38@users.noreply.github.com>
2020-03-02 10:31:54 +0900

    chidori: add up/down key

--- keyboards/chidori/keymaps/1m38/keymap.c
+++ keyboards/chidori/keymaps/1m38/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M, JP_COMM,  JP_DOT, KC_MINS, JP_SLSH,\
   //|--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, ALT_ESC,  LOW_MH,  KC_SPC,           KC_ENT, RAI_HK,  KC_RSFT, XXXXXXX, XXXXXXX, TG(_NUMPAD) \
+      XXXXXXX, XXXXXXX, XXXXXXX, ALT_ESC,  LOW_MH,  KC_SPC,           KC_ENT, RAI_HK,  KC_RSFT, KC_DOWN,   KC_UP, TG(_NUMPAD) \
   //|-----------------------------------------------------'        `-----------------------------------------------------'
   ),
 

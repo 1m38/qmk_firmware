d71184a04b4dd1ac54dfad81b68041178d32c41c
Masaya Uno <13414029+1m38@users.noreply.github.com>
2020-03-04 00:27:47 +0900

    Define Eucalyn layer to crkbd

--- keyboards/crkbd/keymaps/1m38/keymap.c
+++ keyboards/crkbd/keymaps/1m38/keymap.c
@@ -12,14 +12,19 @@ extern uint8_t is_master;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _RAISE 1
-#define _LOWER 2
-#define _NUMPAD 3
-#define _ADJUST 4
+enum layer_number
+{
+  _QWERTY = 0,
+  _EUCALYN,
+  _RAISE,
+  _LOWER,
+  _NUMPAD,
+  _ADJUST
+};
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  EUCALYN,
   RGBRST
 };
 
@@ -56,8 +61,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           ALT_ESC,  LOW_MH,  KC_SPC,     KC_ENT,   RAISE, KC_RSFT \
                                       //`--------------------------'  `--------------------------'
-
   ),
+
+  [_EUCALYN] = LAYOUT( \
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB,    KC_Q,    KC_W, JP_COMM,  JP_DOT, JP_SCLN,                         KC_M,    KC_R,    KC_D,    KC_Y,    KC_P,  KC_BSPC,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      MY_LCTL,    KC_A,    KC_O,    KC_E,    KC_I,    KC_U,                         KC_G,    KC_T,    KC_K,    KC_S,    KC_N, JP_COLN,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_F,                         KC_B,    KC_H,    KC_J,    KC_L, KC_MINS, JP_SLSH,\
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          ALT_ESC,  LOW_MH,  KC_SPC,     KC_ENT,   RAISE, KC_RSFT \
+                                      //`--------------------------'  `--------------------------'
+  ),
+
 
   [_RAISE] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -101,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, G(KC_A), _______, G(KC_D), _______, _______,                   S(MY_CAPS), KC_PSCR, KC_PGUP, _______, _______,   PANIC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_LWIN, G(KC_X), _______, G(KC_V), _______,                      _______, KC_HOME, KC_PGDN,  KC_END, _______, _______,\
+      _______, KC_LWIN, G(KC_X), _______, G(KC_V), _______,                      _______, KC_HOME, KC_PGDN,  KC_END,  QWERTY, EUCALYN,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______,  LOW_MH, _______,    _______,   RAISE, _______ \
                                       //`--------------------------'  `--------------------------'
@@ -195,11 +212,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   switch (keycode) {
-    // case QWERTY:
-    //   if (record->event.pressed) {
-    //     persistent_default_layer_set(1UL<<_QWERTY);
-    //   }
-    //   return false;
+    case QWERTY:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+    case EUCALYN:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_EUCALYN);
+      }
+      return false;
     // case LOWER:
     //   if (record->event.pressed) {
     //     layer_on(_LOWER);
@@ -245,4 +267,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
   }
   return true;
+}
+
+char layer_state_str[24];
+
+const char *read_layer_state(void) {
+  char *base_layer;
+  switch (default_layer_state)
+  {
+    case (1U<<_QWERTY):
+      base_layer = "Qwerty";
+      break;
+    case (1U<<_EUCALYN):
+      base_layer = "Eucalyn";
+      break;
+    default:
+      base_layer = "Undef";
+      break;
+  }
+
+  char *layer_name;
+  if      (layer_state & (1U<<_ADJUST))   layer_name = "Adjust";
+  else if (layer_state & (1U<<_NUMPAD))   layer_name = "NumPad";
+  else if (layer_state & (1U<<_LOWER))    layer_name = "Lower";
+  else if (layer_state & (1U<<_RAISE))    layer_name = "Raise";
+  else if (layer_state == 0)              layer_name = base_layer;
+  else                                    layer_name = "Undef";
+
+  snprintf(layer_state_str, sizeof(layer_state_str), "Layer: %s %ld", layer_name, layer_state);
+  return layer_state_str;
 }

45dc49240cc968bfdaf3bc541090a69f3a484dbd
Masaya Uno <13414029+1m38@users.noreply.github.com>
2020-03-03 23:42:22 +0900

    Fix crkbd's layer state

--- keyboards/crkbd/keymaps/1m38/layer_state_reader.c
+++ keyboards/crkbd/keymaps/1m38/layer_state_reader.c
@@ -4,7 +4,7 @@
 #include "crkbd.h"
 
 // in the future, should use (1U<<_LAYER_NAME) instead, but needs to be moved to keymap,c
-#define L_BASE 0
+// #define L_BASE 0
 #define L_RAISE 2
 #define L_LOWER 4
 #define L_NUMPAD 8
@@ -14,27 +14,14 @@
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
-  switch (layer_state)
-  {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
-    break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
-    break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  case L_NUMPAD:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: NumPad");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
-    break;
-  default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
-  }
+  char *layer_name;
+  if      (layer_state & L_ADJUST)  layer_name = "Adjust";
+  else if (layer_state & L_NUMPAD)  layer_name = "NumPad";
+  else if (layer_state & L_LOWER)   layer_name = "Lower";
+  else if (layer_state & L_RAISE)   layer_name = "Raise";
+  else if (layer_state == 0)        layer_name = "Querty";
+  else                              layer_name = "Undef";
 
+  snprintf(layer_state_str, sizeof(layer_state_str), "Layer: %s %ld", layer_name, layer_state);
   return layer_state_str;
 }

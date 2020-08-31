08667892a7d8cf366f99e0a43d9b6ebd52742629
Masaya Uno <13414029+1m38@users.noreply.github.com>
2020-08-31 20:24:41 +0900

    modify LOWER/RAISE keys behavior of tei44:1m38

---
+++ keyboards/tei44/keymaps/1m38/config.h
@@ -0,0 +1,6 @@
+#define TAPPING_TERM 160
+#define PERMISSIVE_HOLD
+
+#ifdef ENCODER_ENABLE
+  #define ENCODER_RESOLUTION 4
+#endif
--- keyboards/tei44/keymaps/1m38/keymap.c
+++ keyboards/tei44/keymaps/1m38/keymap.c
@@ -25,15 +25,19 @@ enum layer_names {
     _ADJUST
 };
 
+enum tapdance_keycodes {
+    LOWER_MH,    // 1tap: 無変換, hold: MO(_LOWER), 2tap: TG(_LOWER)
+};
+
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
     QWERTY = SAFE_RANGE,
     EUCALYN,
     ESC_MH,
+    RAISE,
 };
 
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+#define TD_LOWMH TD(LOWER_MH)
 #define ALT_ESC LALT_T(KC_ESC)
 #define WINPSCR G(KC_PSCR)        // Win + PrtScr
 #define WINSFTS G(S(KC_S))        // Win + Shift + S
@@ -45,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //         ,--------------------------------------------.                    ,--------------------------------------------.
                     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,\
     //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
-         KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, JP_MHEN,    JP_HENK,    KC_H,    KC_J,    KC_K,    KC_L, JP_SCLN, JP_COLN,\
+         KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_LWIN,     KC_ESC,    KC_H,    KC_J,    KC_K,    KC_L, JP_SCLN, JP_COLN,\
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,    KC_BTN3,    KC_N,    KC_M, JP_COMM,  JP_DOT, KC_MINS, JP_SLSH,\
     //|--------------------------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_LCTL,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RSFT,\
+                                            KC_LCTL,TD_LOWMH,  KC_SPC,     KC_ENT,   RAISE, KC_RSFT,\
              //,--------.                 `--------+--------+--------'  `--------+--------+--------'                 ,--------.
                  JP_MHEN,                            ALT_ESC,                      KC_BSPC,                            JP_HENK
              //`--------'                          `--------'                    `--------'                          `--------'
@@ -122,7 +126,14 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+uint16_t timer_raise_pressed = 0;
+bool is_last_pressed_key_raise = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed)
+    {
+        is_last_pressed_key_raise = (keycode == RAISE);
+    }
     switch (keycode) {
         case QWERTY:
             if (record->event.pressed) {
@@ -140,9 +151,90 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(JP_MHEN);
             }
             return false;
+        case RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                timer_raise_pressed = record->event.time;
+            } else {
+                layer_off(_RAISE);
+                if (is_last_pressed_key_raise && timer_elapsed(timer_raise_pressed) < TAPPING_TERM) {
+                    // tapped(released) 変換
+                    tap_code(KC_HENK);
+                }
+            }
+            return false;
     }
     return true;
 }
+
+// Tap Dance
+
+typedef enum {
+    UNKNOWN_TAP,
+    SINGLE_TAP,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    TRIPLE_OR_MORE_TAP
+} tap_state_t;
+
+typedef struct {
+    bool        is_press_action;
+    tap_state_t state;
+} tap;
+
+// Determine the current tap dance state
+tap_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (!state->pressed) {
+            return SINGLE_TAP;
+        } else {
+            return SINGLE_HOLD;
+        }
+    } else if (state->count == 2) {
+        return DOUBLE_TAP;
+    } else if (state->count >= 3) {
+        return TRIPLE_OR_MORE_TAP;
+    }
+    return UNKNOWN_TAP;
+}
+
+// Tap Dance definition: LOWER_MH
+static tap lower_mh_tap_state = {
+    .is_press_action = true,
+    .state = UNKNOWN_TAP
+};
+
+void lower_mh_each(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->pressed)
+    {
+        layer_on(_LOWER);
+    }
+}
+
+void lower_mh_finished(qk_tap_dance_state_t *state, void *user_data) {
+    lower_mh_tap_state.state = cur_dance(state);
+    switch (lower_mh_tap_state.state) {
+        case SINGLE_TAP:
+            // 無変換
+            tap_code(KC_MHEN);
+            break;
+        default:
+            break;
+    }
+}
+
+void lower_mh_reset(qk_tap_dance_state_t *state, void *user_data) {
+    if (lower_mh_tap_state.state != DOUBLE_TAP) {
+        layer_off(_LOWER);
+    }
+    lower_mh_tap_state.state = UNKNOWN_TAP;
+}
+
+// Associate our tap dance key with its functionality
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [LOWER_MH] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(
+        lower_mh_each, lower_mh_finished, lower_mh_reset, 200),
+};
 
 #ifdef ENCODER_ENABLE
 void encoder_update_user(uint8_t index, bool clockwise) {
